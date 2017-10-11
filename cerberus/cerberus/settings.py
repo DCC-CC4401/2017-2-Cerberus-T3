d868721a989607f47df6ff85993959739951ebae
Iavallejos <iavallejos93@gmail.com>
2017-10-11 18:56:27 -0300

    Arregladas referencias y creada view de muestra

--- cerberus/cerberus/settings.py
+++ cerberus/cerberus/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cholito.apps.CholitoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
