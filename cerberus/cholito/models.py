4206cfe7ac9f62488e080004d5e5d9ec54bdbb12
Iavallejos <iavallejos93@gmail.com>
2017-10-17 16:17:12 -0300

    Revert "Agregadas tablas de Animales y tipos de animales"
    This reverts commit 016ae9a5447820d1b4af66f3b3bf49188ac4ede9.
    
    Revertido error de agregar feature a master

--- cerberus/cholito/models.py
+++ cerberus/cholito/models.py
@@ -1,20 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class AnimalType(models.Model):
-    type = models.CharField(max_length=50)
-
-
-class Animal(models.Model):
-    name = models.CharField(max_length=50)
-    type = models.ForeignKey('AnimalType', on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='animal_pictures/')
-    sex = (
-        ('M', 'Masculino'),
-        ('F', 'Femenino')
-    )
-    age = models.PositiveSmallIntegerField()
-    adoption_time = models.DateField(auto_now_add=True)
-
