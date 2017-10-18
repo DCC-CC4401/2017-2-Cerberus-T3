c16102b33dda4f52594029578c96d87f1ac70362
Iavallejos <iavallejos93@gmail.com>
2017-10-18 20:46:34 -0300

    Base de datos de Animales
    Agregada tabla para tipo de animales
    Agregada tabla para animales
    Agregado superusuario para el manejo de la base de datos
    Agregado el archivo correspondiente a la base de datos con un ejemplo de animal y 2 ejemplos de tipos de animales
    Agregada imagen correspondiente a ejemplo de animal

--- cerberus/cholito/models.py
+++ cerberus/cholito/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+sex_choices = (
+    ('M', 'Masculino'),
+    ('F', 'Femenino')
+)
+
+
+class TipoDeAnimal(models.Model):
+    tipo = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Tipo de animal"
+        verbose_name_plural = 'Tipos de animales'
+
+    def __str__(self):
+        return self.tipo
+
+
+class Animal(models.Model):
+    nombre = models.CharField(max_length=50)
+    tipo = models.ForeignKey('TipoDeAnimal', on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to='animal_pictures/')
+    sexo = models.CharField(choices=sex_choices, max_length=1)
+    edad = models.PositiveSmallIntegerField()
+    fechaAdopcion = models.DateField(verbose_name='En adopción desde', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Animal'
+        verbose_name_plural = 'Animales'
+
+    def __str__(self):
+        return self.nombre
